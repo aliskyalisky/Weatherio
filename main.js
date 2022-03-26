46fad63e95c14380364fd7c0e8b7165c083bed44
aliskyalisky <aleksi.liski@gmail.com>
2022-03-26 14:34:47 +0200

    bugfixing

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ async function getLocation() {
     let search = document.querySelector('.locationsearch').value
     search === '' ? search = 'helsinki' : search;
     document.querySelector('.locationsearch').value = '';
-    let locationFetch = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=957f632d866171f2c36adfb983e594db`, {mode: 'cors'});
+    let locationFetch = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=957f632d866171f2c36adfb983e594db`, {mode: 'cors'});
     let locationData = await locationFetch.json();
     return {
         lat: locationData[0].lat,
